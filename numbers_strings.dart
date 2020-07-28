0784568e7f494db6dc554216cc9c51355d2118ed
David Toledo <dtoledoz0207@gmail.com>
2020-07-28 10:33:49 -0500

    numbers-strings

---
+++ numbers_strings.dart
@@ -0,0 +1,19 @@
+
+void main() {
+
+  // Numbers
+  int students = 10;
+  double pi = 3.1416;
+  var number = 2.0;
+
+  print('$students - $pi - $number');
+
+  print('*'*30);
+
+  // Strings
+  String name = 'Alejandro';
+
+  print(name);
+  print(name[0]);
+  print(name[name.length - 1]);
+}
